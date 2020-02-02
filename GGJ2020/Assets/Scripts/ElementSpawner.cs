fbbf7fe38b99c397ef9feec2ab3786946997b235
Ryan Schaefer <ryanlschaefer@gmail.com>
2020-02-01 22:20:04 -0600

    Scene optimization, pinch action event.

---
+++ GGJ2020/Assets/Scripts/ElementSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementSpawner : PinchActionTarget
+{
+    [SerializeField] GameObject obj_elementToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public override void OnPinchAction(PinchGrabber pincher)
+    {
+        base.OnPinchAction(pincher);
+
+
+    }
+}
